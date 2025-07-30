8b47841290f6a8a54356c9deaa0441c7875c8127
hackeruser240 <afaqahmad240@yahoo.com>
2025-07-31 02:26:36 +0500

    mod: deleting build & dist before building

--- renamingMedia/build.bat
+++ renamingMedia/build.bat
@@ -1,1 +1,12 @@
-pyinstaller --clean --onefile --noconsole --name="Renaming Media" --add-binary "C:\Users\HP\AppData\Local\Programs\Python\Python312\DLLs\pyexpat.pyd;," renamingMedia.py
+@echo off
+
+rem Delete existing build and dist folders if they exist
+echo Cleaning previous build artifacts...
+rmdir /S /Q "build" 2>nul
+rmdir /S /Q "dist" 2>nul
+echo Cleaning complete.
+
+rem Run PyInstaller command
+pyinstaller --clean --onefile --noconsole --name="Renaming Media" --add-binary "C:\Users\HP\AppData\Local\Programs\Python\Python312\DLLs\pyexpat.pyd;." renamingMedia.py
+
+echo Build process finished.
